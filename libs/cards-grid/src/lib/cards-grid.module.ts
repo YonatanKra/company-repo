d0cba65f2865a8b80a611f5a8b5d6cfa1f1b58c3
yonatankra <yonatan.kra@vonage.com>
2020-12-07 16:33:40 +0200

    Add the cards-grid and card components

--- libs/cards-grid/src/lib/cards-grid.module.ts
+++ libs/cards-grid/src/lib/cards-grid.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardsGridComponent } from './cards-grid/cards-grid.component';
+import { CardComponent } from './cards-grid/card/card.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [CardsGridComponent, CardComponent],
 })
 export class CardsGridModule {}
---
+++ libs/cards-grid/src/lib/cards-grid/cards-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'company-repo-cards-grid',
+  templateUrl: './cards-grid.component.html',
+  styleUrls: ['./cards-grid.component.scss']
+})
+export class CardsGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
