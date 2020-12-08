5e11a00b23a89b1736e00cb0ee5ef2d92884b07c
yonatankra <yonatan.kra@vonage.com>
2020-12-08 13:08:13 +0200

    Use the sticky scroll for the app component

--- apps/unicorn-hunt/src/app/app.component.spec.ts
+++ apps/unicorn-hunt/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
@@ -8,6 +8,7 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [HttpClientModule],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
--- apps/unicorn-hunt/src/app/app.module.ts
+++ apps/unicorn-hunt/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CardsGridModule } from '@unicorn/cards-grid';
+import { StickyScrollModule } from '@unicorn/sticky-scroll';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, CardsGridModule],
+  imports: [BrowserModule, HttpClientModule, CardsGridModule, StickyScrollModule],
   providers: [],
   bootstrap: [AppComponent],
 })
